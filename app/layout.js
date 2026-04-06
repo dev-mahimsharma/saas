15f3f925a6890990d949125e14c1e6cb085ffc40
= <mahimsharma.official@gmail.com>
2026-04-06 08:51:31 +0530

    fix : layout.js fix and fonts added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Providers from "@/components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-slate-50 text-slate-900">
-        <AppLayoutManager>{children}</AppLayoutManager>
+        <Providers>
+          <AppLayoutManager>{children}</AppLayoutManager>
+        </Providers>
       </body>
     </html>
   );
